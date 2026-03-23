02ce5ab44d85c062024ee206597ab0eb27a1062e
Maximo <Maximokugler9@gmail.com>
2026-03-23 20:45:51 -0300

    adjustment for deployment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://inventory-celestina-vwj7.vercel.app"],
     credentials: true,
   })
 );
