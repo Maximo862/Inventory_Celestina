7a5041d04eb3cfb103eef453ed3e7ea6db6becc1
Maximo <Maximokugler9@gmail.com>
2026-03-25 16:07:08 -0300

    adjustment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://inventory-celestina-vwj7.vercel.app"],
+    origin: ["http://localhost:5173", "https://inventory-celestina.vercel.app"],
     credentials: true,
   })
 );
